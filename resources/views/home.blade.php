22c9399708ce2c1605deebfbd64f4cf191a407c3
SimoneVilardo <simovila95@gmail.com>
2023-07-21 15:02:24 +0200

    modificato link riga 16 con fortawesome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,8 @@
         <!-- Styles -->
         @vite('resources/js/app.js')
 
-        <!-- link fontawesome -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <!-- link fortawsome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
     </head>
 
     <body>
@@ -77,6 +77,9 @@
                                 </li>
                             </ul>
                         </div>
+                    </div>
+                    <div class="col-2">
+
                     </div>
                 </div>
             </div>
