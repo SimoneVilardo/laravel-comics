0a676881ba0aa2d660148bb63ed7c7ab5f15e638
SimoneVilardo <simovila95@gmail.com>
2023-07-21 16:55:09 +0200

    main parte comics completata

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,24 @@
         @include('partials.header')
         <main>
             <img class="jumbotron" src="{{ Vite::asset('resources/img/jumbotron.jpg') }}">
-            
+            <div class="bg-black">
+                <div class="container">
+                    <div class="row py-5 position">
+                        @foreach($comics as $comic)
+                            <div class="col-2 py-3">
+                                <div class="card bg-black">
+                                    <img class="dimension" src="{{ $comic['thumb'] }}">
+                                    <p class="text-white">{{ $comic['series'] }}</p>
+                                </div>
+                            </div>
+                        @endforeach
+                        <div class="d-flex justify-content-center">
+                            <button type="button">LOAD MORE</button>
+                        </div>
+                        <div class="col-2 text-center bage">CURRENT SERIES</div>
+                    </div>
+                </div>
+            </div>
         </main>
     </body>
 
