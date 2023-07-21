9b20a8eed0e13ccd8b3914c80d14a3c9d4b79b97
SimoneVilardo <simovila95@gmail.com>
2023-07-21 18:20:09 +0200

    modica padding footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -116,7 +116,7 @@
             </div>
             <div class="bg-footer">
                 <div class="container">
-                    <div class="row">
+                    <div class="row py-4">
                         <div class="col-12 d-flex justify-content-between">
                             <button>SIGN-UP NOW!</button>
                             <div class="icone">
