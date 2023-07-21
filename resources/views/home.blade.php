94bc5013e070dddde0a83a8b4a7a317bb614142c
SimoneVilardo <simovila95@gmail.com>
2023-07-21 14:58:58 +0200

    inserimento logo e link header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,10 +13,74 @@
         <!-- Styles -->
         @vite('resources/js/app.js')
 
+        <!-- link fontawesome -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     </head>
 
     <body>
-
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-10 d-flex justify-content-center align-items-center mt-4">
+                        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}">
+                        <div class="navbar-nav container navbar-light">
+                            <ul class="list-unstyled d-flex justify-content-center text-uppercase ">
+                                <li class="nav-link px-2 py-2">
+                                    <a class="nav-link {{ Route::currentRouteName() === 'homepage' ? 'fw-bold active' : '' }}" href="{{ route('homepage') }}""  href="{{ route('homepage') }}">
+                                        characters
+                                    </a>
+                                </li>
+                                <li class="nav-link px-2 py-2">
+                                    <a class="nav-link">
+                                        comics
+                                    </a>
+                                </li>
+                                <li class="nav-link px-2 py-2">
+                                    <a class="nav-link">
+                                        movies
+                                    </a>
+                                </li>
+                                <li class="nav-link px-2 py-2">
+                                    <a class="nav-link">
+                                        tv
+                                    </a>
+                                </li>
+                                <li class="nav-link px-2 py-2">
+                                    <a class="nav-link">
+                                        games
+                                    </a>
+                                </li>
+                                <li class="nav-link px-2 py-2">
+                                    <a class="nav-link">
+                                        collectibiles
+                                    </a>
+                                </li>
+                                <li class="nav-link px-2 py-2">
+                                    <a class="nav-link">
+                                        videos
+                                    </a>
+                                </li>
+                                <li class="nav-link px-2 py-2">
+                                    <a class="nav-link">
+                                        fans
+                                    </a>
+                                </li>
+                                <li class="nav-link px-2 py-2">
+                                    <a class="nav-link">
+                                        news
+                                    </a>
+                                </li>
+                                <li class="nav-link px-2 py-2">
+                                    <a class="nav-link">
+                                        shop 
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
     </body>
 
 </html>
