2bd1491cb38579c046f8809f97847dc1cd392904
SimoneVilardo <simovila95@gmail.com>
2023-07-21 13:40:21 +0200

    inserita cartella img

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+        <title>Laravel DC Comics</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <!-- Styles -->
-    @vite('resources/js/app.js')
+        <!-- Styles -->
+        @vite('resources/js/app.js')
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-</body>
+    </body>
 
 </html>
